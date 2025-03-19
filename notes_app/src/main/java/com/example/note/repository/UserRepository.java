749707b4d63970c2e4ac97bb6c7020b3d761e19d
Meerim K <thvrim@gmail.com>
2025-03-19 16:30:36 +0600

    added repository interfaces: UserRepository, TaskRepository

---
+++ notes_app/src/main/java/com/example/note/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.note.repository;
+
+import com.example.note.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
